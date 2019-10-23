1e42d22c84201cb07a8b337be9f8a71768997c5f
Sam Schiavone <sam.schiavone@gmail.com>
2019-10-22 20:33:36 -0400

    jeroen thesis and new example

---
+++ Examples/JacobianEqnsExample.m
@@ -0,0 +1,9 @@
+// Example for Jacobian equations
+
+load "Code/KummerFunctions.m";
+
+QQ := Rationals();
+R<x> := PolynomialRing(QQ);
+f := x*(x-1)*(x-2)*(x-3)*(x-4)*(x-5);
+X := HyperellipticCurve(f);
+CantorEquations(X);
